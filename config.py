769626a540b672e5ce47d484fa5655e85c9aa533
sumanbgl <suman.bgl@gmail.com>
2021-04-07 23:27:54 +0530

    second commit.

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ class Config(object):
 
     CLIENT_ID = os.environ.get('CLIENT_ID') or 'ENTER_CLIENT_ID_HERE'
 
-    REDIRECT_PATH = "/getCMSToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
+    REDIRECT_PATH = "/incexp"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
 
     # You can find the proper permission names from this document
     # https://docs.microsoft.com/en-us/graph/permissions-reference
